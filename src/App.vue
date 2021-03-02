e99103eec62295469ff44984d5cb05edf531c097
yachao zhang <yachaozhang@yachaodeMacBook-Pro.local>
2021-03-02 23:50:40 +0800

    v-for demo

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <list-demo></list-demo>
 </template>
 
 <script lang="ts">
 import { defineComponent, provide, ref } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
+import ListDemo from "./components/listDemo.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     HelloWorld,
+    ListDemo,
   },
-  
+
   setup() {
     const theme = ref(11);
     /*
---
+++ src/components/listDemo.vue
@@ -0,0 +1,45 @@
+<!-- v-for 集中抽离-->
+<template>
+  <div>
+    <ul>
+      <li
+        v-for="(item, index) in state.stus"
+        :key="item.id"
+        @click="remStu(index)"
+      >
+        {{ item.name }}---{{ item.age }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  name: "listDemo",
+  setup() {
+    let { state, remStu } = useRemoveStudent();
+    return { state, remStu };
+  },
+};
+
+// 把相同业务进行抽离
+function useRemoveStudent() {
+  let state = reactive({
+    stus: [
+      { id: 1, name: "zs", age: 10 },
+      { id: 2, name: "ls", age: 20 },
+      { id: 3, name: "ww", age: 30 },
+      { id: 4, name: "zl", age: 40 },
+    ],
+  });
+
+  function remStu(index) {
+    state.stus = state.stus.filter((stu, idx) => idx !== index);
+  }
+  return { state, remStu };
+}
+</script>
+
+<style>
+</style>    
