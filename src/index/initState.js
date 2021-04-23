d34848ebe67732eec71197cae0112d63ec235696
张亚超 <451142214@qq.com>
2021-04-23 10:54:30 +0800

    vue2data代理

--- src/index/initState.js
+++ src/index/initState.js
@@ -20,6 +20,17 @@ export function initState(vm) {
   // }
 }
 
+function proxy(vm, source, key) {
+  Object.defineProperty(vm, key, {
+    get() {
+      return vm[source][key]
+    },
+    set(newVal) {
+      vm[source][key] = newVal
+    }
+  })
+}
+
 
 function initData(vm) {
   let data = vm.$options.data
@@ -28,8 +39,12 @@ function initData(vm) {
   // data 是对象直接用  是函数使用返回值
   //将data与vm关联起来
   data = vm._data = isFunction(data) ? data.call(vm) : data
+
+  //用户去vm.xxx 上面取值   实际上是取vm._data.xx的值
+  for (let key in data) {
+    proxy(vm, '_data', key)
+  }
+
   // 劫持数据 观测数据
   observer(data)
-
-  console.log(vm)
 }
