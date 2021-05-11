73112ce97968ac4b4027e9cf10c9feedebad23ae
Lrainw <451142214@qq.com>
2021-05-11 19:30:47 +0800

    promise版本1

--- src/components/myPromise.js
+++ src/components/myPromise.js
@@ -1,3 +1,21 @@
-function MyPromise(callBack) {}
+function MyPromise(callBack) {
+  var _this = this
+  _this.value = void 0
+  var onResolvedCallbacks //promise resolve回掉函数
+  var onRejectedCallback //promise reject回调函数
 
+  //resolve处理函数
+  _this.resolve = function (value) {
+    onResolvedCallbacks()
+  }
+
+  //reject处理函数
+  _this.reject = function () {
+    onRejectedCallback()
+  }
+
+  callBack(_this.resolve, _this.reject) //执行callback并传入相应的值
+}
+
+// 添加then方法
 MyPromise.prototype.then = function () {}
