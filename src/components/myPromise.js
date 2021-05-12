af86ebb3f1ca5fc84bf860b1313ed98230d9d86c
Lrainw <451142214@qq.com>
2021-05-12 14:39:48 +0800

    promise架子

--- src/components/myPromise.js
+++ src/components/myPromise.js
@@ -12,6 +12,11 @@ function MyPromise(callBack) {
 
   //resolve处理函数
   _this.resolve = function (value) {
+    if (value instanceof MyPromise) {
+      // 如果value是promis 递归执行
+      return value.then(_this.resolve, _this.reject)
+    }
+
     setTimeout(() => {
       if (_this.currentState === PENDING) {
         _this.currentState = REJECTED //状态管理
@@ -32,12 +37,69 @@ function MyPromise(callBack) {
     }, 0)
   }
 
-  callBack(_this.resolve, _this.reject) //执行callback并传入相应的值
+  try {
+    callBack(_this.resolve, _this.reject) //执行callback并传入相应的值
+  } catch (error) {
+    _this.reject(error)
+  }
 }
 
-// 添加then方法
-MyPromise.prototype.then = function () {}
+// 添加then方法 接受两个参数，成功的回调，失败的回调
+MyPromise.prototype.then = function (onFulfilled, onRejected) {
+  var _this = this
+  //规范2.27规定 then必须返回一个新的promise
+  var promise2
+
+  onFulfilled =
+    typeof onFulfilled === 'function' ? onFulfilled : (value) => value
+  onRejected =
+    typeof onRejected === 'function'
+      ? onRejected
+      : (error) => {
+          throw error
+        }
+
+  if (_this.currentState === REJECTED) {
+    return (promise2 = new MyPromise(function (resolve, reject) {}))
+  }
+
+  if (_this.currentState === PENDING) {
+    return (promise2 = new MyPromise(function (resolve, reject) {
+      _this.onResolvedCallbacks.push(function () {})
+    }))
+  }
+
+  if (_this.currentState === FULFILLED) {
+    return (promise2 = new MyPromise(function (resolve, reject) {}))
+  }
+}
+
+MyPromise.prototype.catch = function (onRejected) {
+  return this.then(null, onRejected)
+}
+
+MyPromise.prototype.finally = function (callBack) {
+  return this.then(
+    function () {
+      return MyPromise.resolve(callBack()).then(function () {
+        return value
+      })
+    },
+    function (err) {
+      return MyPromise.resolve(callBack()).then(function () {
+        throw err
+      })
+    },
+  )
+}
 
 /*
 promise连续执行两个或者多个异步操作，在这种情况下每一个后来的操作都是在前面的操作结果执行成功之后带着上一步的返回结果开始执行的
 */
+
+class Promise {
+  constructor() {
+    this.name = 'hahaha'
+    console.log(this)
+  }
+}
